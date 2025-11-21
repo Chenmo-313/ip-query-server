f0b61a4a81d31097a425a64b06fc886d3176c968
Chenmo_313 <122888325+Chenmo-313@users.noreply.github.com>
2025-11-21 10:46:01 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,38 +1,35 @@
-document.getElementById("queryBtn").addEventListener("click", loadIPInfo);
+document.getElementById("btn").addEventListener("click", loadIPInfo);
 
 async function loadIPInfo() {
-  document.getElementById("info").innerHTML = "正在查询...";
+  const infoEl = document.getElementById("info");
+  infoEl.innerHTML = "正在查询...";
+
   try {
     const res = await fetch("/api/ip");
     const data = await res.json();
 
-    document.getElementById("info").innerHTML = `
-      <b>IP 地址：</b> ${data.ip}<br>
-      <b>城市：</b> ${data.city || "未知"}<br>
-      <b>地区：</b> ${data.region || "未知"}<br>
-      <b>国家：</b> ${data.country || "未知"}<br>
-      <b>位置：</b> ${data.loc || "未知"}<br>
-      <b>ISP：</b> ${data.org || "未知"}<br>
-      <b>邮编：</b> ${data.postal || "未知"}<br>
+    infoEl.innerHTML = `
+      <b>IP 地址：</b>${data.ip}<br>
+      <b>城市：</b>${data.city || "未知"}<br>
+      <b>地区：</b>${data.region || "未知"}<br>
+      <b>国家：</b>${data.country || "未知"}<br>
+      <b>位置：</b>${data.loc || "未知"}<br>
     `;
 
     if (data.loc) {
       const [lat, lon] = data.loc.split(",");
       showMap(lat, lon);
-    } else {
-      showMap(0, 0);
     }
   } catch (err) {
     console.error(err);
-    document.getElementById("info").innerHTML = "<b>无法获取 IP 信息</b>";
-    showMap(0, 0);
+    infoEl.innerHTML = "无法获取 IP 信息";
   }
 }
 
 function showMap(lat, lon) {
-  const map = L.map("map").setView([lat, lon], 5);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-  }).addTo(map);
+  const mapEl = document.getElementById("map");
+  mapEl.innerHTML = ""; // 清空地图
+  const map = L.map("map").setView([lat, lon], 13);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom: 19 }).addTo(map);
   L.marker([lat, lon]).addTo(map).bindPopup("你的位置").openPopup();
 }

499888d9b8e0b7b0c046bd2a848575ef0b88c307
Chenmo_313 <122888325+Chenmo-313@users.noreply.github.com>
2025-11-21 10:52:35 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,35 +1,35 @@
-document.getElementById("btn").addEventListener("click", loadIPInfo);
-
-async function loadIPInfo() {
-  const infoEl = document.getElementById("info");
-  infoEl.innerHTML = "正在查询...";
-
+// 获取公网 IP
+async function getCurrentIP() {
   try {
-    const res = await fetch("/api/ip");
+    const res = await fetch("https://api.ipify.org?format=json");
     const data = await res.json();
-
-    infoEl.innerHTML = `
-      <b>IP 地址：</b>${data.ip}<br>
-      <b>城市：</b>${data.city || "未知"}<br>
-      <b>地区：</b>${data.region || "未知"}<br>
-      <b>国家：</b>${data.country || "未知"}<br>
-      <b>位置：</b>${data.loc || "未知"}<br>
-    `;
-
-    if (data.loc) {
-      const [lat, lon] = data.loc.split(",");
-      showMap(lat, lon);
-    }
-  } catch (err) {
-    console.error(err);
-    infoEl.innerHTML = "无法获取 IP 信息";
+    return data.ip;
+  } catch (error) {
+    console.error("获取公网 IP 错误：", error);
+    return null;
   }
 }
 
-function showMap(lat, lon) {
-  const mapEl = document.getElementById("map");
-  mapEl.innerHTML = ""; // 清空地图
-  const map = L.map("map").setView([lat, lon], 13);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom: 19 }).addTo(map);
-  L.marker([lat, lon]).addTo(map).bindPopup("你的位置").openPopup();
+// 查询 IP 信息
+async function loadIPInfo() {
+  const ip = await getCurrentIP();
+  if (!ip) {
+    document.getElementById("info").innerHTML = "无法获取公网 IP";
+    return;
+  }
+
+  const res = await fetch(`/api/ip?ip=${ip}`);
+  const data = await res.json();
+  document.getElementById("info").innerHTML = `
+    <b>IP 地址：</b>${data.ip}<br>
+    <b>城市：</b>${data.city || "未知"}<br>
+    <b>地区：</b>${data.region || "未知"}<br>
+    <b>国家：</b>${data.country || "未知"}<br>
+    <b>位置：</b>${data.loc || "未知"}<br>
+  `;
+
+  if (data.loc) {
+    const [lat, lon] = data.loc.split(",");
+    showMap(lat, lon);
+  }
 }
